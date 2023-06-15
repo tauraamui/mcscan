525ce82b8e76f6a2f23645d6b07ddca0720e7849
tauraamui <adampstringer@protonmail.com>
2023-06-15 17:28:50 +0100

    feat: implement new cmd which lists all MC save worlds

---
+++ cmd/list/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	stdos "os"
+	"path/filepath"
+
+	"github.com/hack-pad/hackpadfs/os"
+	"github.com/tauraamui/mcscan/internal/scan"
+	"github.com/tauraamui/mcscan/vfs"
+)
+
+// ~/Library/Application Support/minecraft
+
+func main() {
+	fsys := must(resolveFS(must(stdos.UserConfigDir())))
+
+	mcSavesPath := filepath.Join("minecraft", "saves")
+	fi := must(fsys.Stat(mcSavesPath))
+	if fi.IsDir() {
+		worldDirs := must(fsys.ReadDir(mcSavesPath))
+
+		for _, wdir := range worldDirs {
+			name := wdir.Name()
+			if !vfs.IsHidden(name) {
+				fmt.Println(name)
+			}
+		}
+	}
+}
+
+func resolveWorlds(fsResolver func() (*os.FS, error)) []scan.World {
+	return nil
+}
+
+func resolveFS(base string) (*os.FS, error) {
+	fs := os.NewFS()
+
+	baseDirectory := must(fs.FromOSPath(base)) // Convert to an FS path
+	baseDirFS := must(fs.Sub(baseDirectory))   // Run all file system operations rooted at the current working directory
+
+	ofs, ok := baseDirFS.(*os.FS)
+	if !ok {
+		return nil, errors.New("sub FS not an OS instance FS")
+	}
+
+	return ofs, nil
+}
+
+func must[T any](v T, err error) T {
+	if err != nil {
+		_, _ = fmt.Fprintln(stdos.Stderr, err)
+		stdos.Exit(1)
+	}
+	return v
+}
