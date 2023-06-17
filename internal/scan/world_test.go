46cc98bd885daafa87b834534e8afce898fb10a3
tauraamui <adampstringer@protonmail.com>
2023-06-17 01:58:15 +0100

    test: define basic test so ensure world opening works

---
+++ internal/scan/world_test.go
@@ -0,0 +1,26 @@
+package scan_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	"github.com/matryer/is"
+	"github.com/tauraamui/mcscan/internal/scan"
+)
+
+func buildMockFS() fstest.MapFS {
+	return fstest.MapFS{
+		"config/minecraft/test world/region/r.0.0.mca": &fstest.MapFile{
+			Data: []byte{},
+		},
+	}
+}
+
+func TestWorldOpenReturnsWorldReferenceWithNoError(t *testing.T) {
+	is := is.New(t)
+	world, err := scan.OpenWorld(buildMockFS(), "config/minecraft/test world")
+	is.NoErr(err)
+
+	is.True(world != nil)
+	is.Equal(world.Name(), "test world")
+}
