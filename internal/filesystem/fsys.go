9a8d2ddeff2674149ed237f3e357bbd6a0336f28
tauraamui <adampstringer@protonmail.com>
2023-06-22 12:41:04 +0100

    remove requirement of direct dep to hackpadfs to use filesystem interface

--- internal/filesystem/fsys.go
+++ internal/filesystem/fsys.go
@@ -2,13 +2,11 @@ package filesystem
 
 import (
 	"io/fs"
-
-	"github.com/hack-pad/hackpadfs"
 )
 
 type FS interface {
 	Open(name string) (fs.File, error)
 	fs.StatFS
 	fs.ReadDirFS
-	hackpadfs.WriteFileFS
+	WriteFile(name string, data []byte, perm fs.FileMode) error
 }
